30d261624540cf0ea0afd71456c63cdb10f96e05
Tafadzwa Mashakada <tmashakada10@gmail.com>
2023-11-04 10:44:45 +0200

    initial commit

---
+++ src/main/java/com/mashreq/ConferenceRoomBookingAPI/ConferenceRoomBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.mashreq.ConferenceRoomBookingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mashreq/ConferenceRoomBookingAPI/ConferenceRoomBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashreq.ConferenceRoomBookingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
