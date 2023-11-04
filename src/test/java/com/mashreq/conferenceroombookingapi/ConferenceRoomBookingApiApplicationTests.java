2c6d83f92372448ca869a3a804bfac88c2b9d5c6
Tafadzwa Mashakada <tmashakada10@gmail.com>
2023-11-04 10:58:06 +0200

    add packages

--- src/test/java/com/mashreq/conferenceroombookingapi/ConferenceRoomBookingApiApplicationTests.java
+++ src/test/java/com/mashreq/conferenceroombookingapi/ConferenceRoomBookingApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mashreq.ConferenceRoomBookingAPI;
+package com.mashreq.conferenceroombookingapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
